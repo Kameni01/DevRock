69d8261465b28121d816d3d9a6f3b4cb0b401a84
Kameni01 <kameni01@mail.ru>
2019-01-27 00:03:36 +0500

    debug

--- DevRock/settings.py
+++ DevRock/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'DevRock.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DevRock',
+        'NAME': 'devrock',
         'USER': 'admin',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
