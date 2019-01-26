a56d34cacdbc6666dae5c2834e6adfb879630286
Kameni01 <kameni01@mail.ru>
2019-01-26 20:13:12 +0500

    change

--- startdir/models.py
+++ startdir/models.py
@@ -6,4 +6,4 @@ User = get_user_model()
 class profile(models.Model):
     user = models.ForeignKey(User, verbose_name='Псевдоним', on_delete=models.CASCADE)
     username = models.CharField(max_length=40, verbose_name='Имя пользователя', on_delete=models.CASCADE)
-    userfamily = models.
+    #userfamily = models.
