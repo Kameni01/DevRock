1ab6065be8412be2a5d087c6faaf10b87df83532
Andrey Belianin <belyaninwork@gmail.com>
2019-02-06 02:14:32 +0500

    Task model completed + Task comments
    probably doesn't work

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,34 +1,36 @@
 from django.db import models
-from django.shortcuts import reverse
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from mezzanine.models import Projects
-from .utils import get_all_projects_enumerated
-
-
 
 
 class Task(models.Model):
     TODO = 0
-    INPROGRESS = 1
+    IN_PROGRESS = 1
     REVIEW = 2
     DONE = 3
-    STATUSES = ((TODO, 'ToDo',), (INPROGRESS, 'InProgress',),
+    STATUSES = ((TODO, 'ToDo',), (IN_PROGRESS, 'InProgress',),
                     (REVIEW, 'Review',), (DONE, 'Done',))
-
-    DEFAULT_PROJECT = "Нет"
 
     title = models.CharField(max_length=160, db_index=True)
     body = models.TextField(blank=True)
     status = models.IntegerField(default=TODO, choices=STATUSES)
-    project = models.TextField(choices=get_all_projects_enumerated(), null=True, default=DEFAULT_PROJECT)
+    project = models.ForeignKey(Projects, null=True, on_delete=models.CASCADE)
 
     creation_date = models.DateTimeField(auto_now_add=True)
     deadline_date = models.DateTimeField(auto_now_add=True)
 
-    creator = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE, null=True)
-    # reviewer = profile
-    # comments
+    author = models.ForeignKey(User, verbose_name='Автор', related_name="Task_author", on_delete=models.SET_NULL, null=True)
+    reviewer = models.ForeignKey(User, verbose_name='Проверяющий', related_name="Task_reviewer", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+
+
+class TaskComment(models.Model):
+    body = models.TextField()
+    task = models.ForeignKey(Task, verbose_name="Комментарий", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.SET_NULL, related_name="TaskComment_author", null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
--- taskmanager/utils.py
+++
@@ -1,9 +0,0 @@
-from mezzanine.models import Projects
-
-
-def get_all_projects_enumerated():
-    choices = [("Нет", "Нет")]
-    for project in Projects.objects.all():
-        choices.append((project.title, project.title))
-
-    return choices
